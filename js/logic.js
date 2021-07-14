00150ce58ead3cf78376b7c14962c29570e050ff
Alan Luo Fanng <afanng24@gmail.com>
2021-07-14 15:45:40 -0400

    Add files via upload

--- js/logic.js
+++ js/logic.js
@@ -21,3 +21,16 @@ L.circle([34.0522, -118.2437], {
     color:"black",
     fillColor: '#ffffa1'
 }).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+});
